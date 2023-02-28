bcbf8442bb3e971e27f2dd870ad0eec60e558a2b
akmelalee <akmelalee@gmail.com>
2023-02-28 12:58:41 +0500

    initial setup bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-bootstrap-ui-components';
+  title = 'Angular Bootstrap UI Components';
 }
